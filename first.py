9693f62f6a598443b4e3f59b7c304721cad14a66
plm9 <axilleas.pats@hotmail.com>
2023-10-26 17:34:23 +0200

    First problem solved

--- first.py
+++ first.py
@@ -1,6 +1,18 @@
-import numpy as np 
 
-print(np.random.uniform(10))
-print("This has been modified")
+import time
+from datetime import date
 
-a=np.array(np.random.uniform(10,12,10))
+name =input("What is your name:")
+age=int(input("What is your age:"))
+
+""" -today= date.today()["year"] """
+today=date.fromtimestamp(time.time())
+
+years100=100-age
+
+print(name+f" you will be 100 years old in:{today.year+years100}")
+
+Prints =int(input("How many times should I print the message:"))
+
+for i in range(Prints):
+    print(name+f" you will be 100 years old in:{today.year+years100}\n")
